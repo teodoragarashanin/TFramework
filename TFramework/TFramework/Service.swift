f5072aff287421179dfdf0db68ac21cd88f7b203
Teodora Garasanin <t.garasanin@levi9.com>
2019-09-16 17:19:52 +0200

    Finished TFramework

---
+++ TFramework/TFramework/Service.swift
@@ -0,0 +1,18 @@
+//
+//  Service.swift
+//  TFramework
+//
+//  Created by Teodora Garasanin on 9/16/19.
+//  Copyright © 2019 Teodora Garasanin. All rights reserved.
+//
+
+import Foundation
+
+public class Service {
+    
+    private init() {}
+    
+    public static func doSomething() -> String {
+        return "Did some stuff."
+    }
+}
---
+++ TFramework/TFramework/TFramework.h
@@ -0,0 +1,19 @@
+//
+//  TFramework.h
+//  TFramework
+//
+//  Created by Teodora Garasanin on 9/16/19.
+//  Copyright © 2019 Teodora Garasanin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TFramework.
+FOUNDATION_EXPORT double TFrameworkVersionNumber;
+
+//! Project version string for TFramework.
+FOUNDATION_EXPORT const unsigned char TFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TFramework/PublicHeader.h>
+
+
