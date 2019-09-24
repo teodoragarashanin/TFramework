c41fb6e7343b2c5c99565a7b70f018dbc876e36e
Teodora Garasanin <t.garasanin@levi9.com>
2019-09-24 11:34:54 +0200

    WebView initialization method

--- TFramework/TFramework/Service.swift
+++ TFramework/TFramework/Service.swift
@@ -7,12 +7,19 @@
 //
 
 import Foundation
+import WebKit
 
 public class Service {
     
     private init() {}
     
-    public static func doSomething() -> String {
-        return "Did some stuff."
+    public static func showWebView(storyboard: UIStoryboard) {
+        let webView = WKWebView(frame: UIScreen.main.bounds)
+        let myURL = URL(string:"https://www.apple.com")
+        let myRequest = URLRequest(url: myURL!)
+        webView.load(myRequest)
+        
+        let vc = storyboard.instantiateInitialViewController()
+        vc?.view.addSubview(webView)
     }
 }
